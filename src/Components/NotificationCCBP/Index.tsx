233388984965a64a0d86de7eb34d8aee4566cdac
isandeepkumar2000 <sandeep.kumarrrr2000@gmail.com>
2023-01-23 11:54:48 +0530

    firsr changes

---
+++ src/Components/NotificationCCBP/Index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotificationAppProps = () => {
+  return <div>NotificationAppProps</div>;
+};
+
+export default NotificationAppProps;
---
+++ src/Components/NotificationCCBP/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotificationApp
+ = () => {
+  return (
+    <div>NotificationApp
+        
+    </div>
+  )
+}
+
+export default NotificationApp
