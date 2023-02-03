e2cec6c49fb08588308ae764fb791404059a42ec
isandeepkumar2000 <121090539+isandeepkumar2000@users.noreply.github.com>
2023-02-03 09:28:33 +0530

    changes

--- src/Components/NxtTrendzAuthenication/Products/index.tsx
+++ src/Components/NxtTrendzAuthenication/Products/index.tsx
@@ -3,8 +3,20 @@ import React from "react";
 
 import HeaderPage from "../Header";
 
+
+const sortbyOption = [ 
+  {
+otionId: 'PRICE_HIGH',
+  displayText: 'Price (High-Low)',
+  },
+   {
+otionId: 'PRICE_LOW',
+  displayText: 'Price (Low-High)',
+  },
+]
+
 const ProductsPage = () => {
-  
+ 
   return (
     <>
       <HeaderPage />
