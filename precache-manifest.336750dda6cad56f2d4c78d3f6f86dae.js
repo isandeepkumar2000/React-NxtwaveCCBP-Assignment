8daa771e3970a186503d9234ed05c0236f35470c
isandeepkumar2000 <sandeep.kumarrrr2000@gmail.com>
2023-02-17 16:14:12 +0530

    Updates

---
+++ precache-manifest.336750dda6cad56f2d4c78d3f6f86dae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ed97d8e514b03cb58c9",
+    "url": "/static/css/main.9defc920.chunk.css"
+  },
+  {
+    "revision": "6ed97d8e514b03cb58c9",
+    "url": "/static/js/main.6ed97d8e.chunk.js"
+  },
+  {
+    "revision": "40a913067904795b6e96",
+    "url": "/static/js/1.40a91306.chunk.js"
+  },
+  {
+    "revision": "3acd511a7667e58a71b7",
+    "url": "/static/js/2.3acd511a.chunk.js"
+  },
+  {
+    "revision": "0f72e73eb2f47e081041",
+    "url": "/static/js/runtime~main.0f72e73e.js"
+  },
+  {
+    "revision": "8d041f5d9c8ae691159bb1367ed7e84f",
+    "url": "/index.html"
+  }
+];
